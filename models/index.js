60ebe54188d53df20364e212fedb1a53ec42f58a
John Crimmings <john.crimmings@gmail.com>
2016-01-23 16:04:04 -0600

    bookshelf/knex notes

---
+++ models/index.js
@@ -0,0 +1,39 @@
+// created by crimmings 1/23
+// playing around with knex/bookshelf
+
+/*
+var knex = require('knex')({
+client: 'pg'
+connection: {
+host: 'localhost',
+user: '',
+password: '',
+database: 'unionpark'
+}
+});
+
+var bookshelf = require('bookshelf')(knex);
+
+schema.js?
+var Schema = {
+    consumercues: {
+        id: {type: "increments", nullable: false, primary: true},
+        name: {type: "string", maxlength: 50, nullable: false, unique: true},
+        status: {look up what we need here},
+        start_date: {type: "date", nullable: false},
+        end_date: {type: "date", nullable: false},
+        description: {type: "string", maxlength: 200, nullable: false}
+        };
+
+    tags: {
+        id: {type: 'increments', nullable: false, primary: true},
+        name: {type: 'string', maxlength: 30, nullable: false}
+        }
+
+     calendars: {
+        id: {type: 'increments', nullable: false, primary: true},
+        name: {type: 'string', maxlength: 30, nullable: false}
+        }
+}//end of schema
+
+ */
