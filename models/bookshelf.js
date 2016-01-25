5f02d38ae1875f57ccaff9f10051d510c11d5054
John Crimmings <john.crimmings@gmail.com>
2016-01-24 20:02:04 -0600

    add bookshelf.js

---
+++ models/bookshelf.js
@@ -0,0 +1,12 @@
+/**
+ * Created by crimmings on 1/24/16.
+ */
+
+/*
+
+Bookshelf library initiliazed by passing an initialized Knex client instance.
+Initialization should only happen once in app.
+
+var knex = require('knex')(dbConfig);
+module.exports = require('bookshelf')(knex);
+ */
