4ee8c1abae10bc009e2f8300f95b544f7e57bc68
John Crimmings <john.crimmings@gmail.com>
2016-01-26 11:50:08 -0600

    check in some initial router db access

--- server/apiRoutes/ccTable.js
+++ server/apiRoutes/ccTable.js
@@ -1,12 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('knex');
 
-router.get('/',function(req,res){
+router.get('/',function(req,res){ //go into db and get
+ /*
+  knex('cues')
+  .select('name', 'status', 'start_date', 'end_date').orderBy('start_date', 'asc')
+  .join('tags', 'cue.id', '=', 'tag.id') // joins tag table. 1st join argument/join operator/second join argument
+  .limit(25).offset(0)
+  .then(function(result){
+ response.json(result);
+ })
+  */
   res.send(200, 'congratz bro get');
 });
+
+//pagination https://github.com/tgriesser/bookshelf/issues/435
+
 
 router.put('/', function(req,res){
   res.send(200, 'congratz bro put');
 });
+
+//manual router put insert data
+
+//delete route (in database switch status field)
 
 module.exports = router;
