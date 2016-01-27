36ffb29cafaa3705bb6804d7cdd82907dc4f7f12
John Crimmings <john.crimmings@gmail.com>
2016-01-26 23:53:53 -0600

    add gcloud, deleted knex.js, made notes in cctable routes

--- server/apiRoutes/ccTable.js
+++ server/apiRoutes/ccTable.js
@@ -1,29 +1,30 @@
 var express = require('express');
 var router = express.Router();
-var knex = require('knex');
+var glcoud = require('gcloud');
 
 router.get('/',function(req,res){ //go into db and get
- /*
-  knex('cues')
-  .select('name', 'status', 'start_date', 'end_date').orderBy('start_date', 'asc')
-  .join('tags', 'cue.id', '=', 'tag.id') // joins tag table. 1st join argument/join operator/second join argument
-  .limit(25).offset(0)
-  .then(function(result){
- response.json(result);
- })
-  */
   res.send(200, 'congratz bro get');
 });
 
-//pagination https://github.com/tgriesser/bookshelf/issues/435
 
 
 router.put('/', function(req,res){
   res.send(200, 'congratz bro put');
 });
 
-//manual router put insert data
-
-//delete route (in database switch status field)
 
 module.exports = router;
+
+
+/** datastore syntax
+
+ router.post
+
+ // save data to database (from form or new tag addition)
+ var data = req.body;
+ model.create(data, function(err, savedData){
+ if(err) {return handleRpcError(err, res); }
+ res.redirect(req.baseUrl + '/' + savedData.id);
+
+
+ gcloud models
