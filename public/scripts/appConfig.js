a36dd4f26ba6ca631aed0e21c5fd91456c70b09b
John Crimmings <john.crimmings@gmail.com>
2016-01-29 17:07:24 -0600

    add getTags

--- public/scripts/appConfig.js
+++ public/scripts/appConfig.js
@@ -1,16 +1,23 @@
+// Angular module
+
+// create our app. injecting ngRoute/ngTagsInput directives
 var app = angular.module('unionPark', ['ngRoute','ngTagsInput']);
 
+// routes
+// when just #/ ... goes to /pages/login.html, then use loginCtrl
 app.config(['$routeProvider', '$locationProvider', '$httpProvider',
   function ($routeProvider, $locationProvider, $httpProvider) {
     $routeProvider.when('/', {
       templateUrl: '/pages/login.html',
       controller: 'loginCtrl'
-    }).
+    }). // when you have #/ccTable...use ccTable.html w/ ccTableCtrl
       when('/ccTable', {
       templateUrl: '/pages/ccTable.html',
       controller: 'ccTableCtrl'
     }).
-    otherwise({
+    otherwise({  // if neither, redirect to root
       redirectTo:'/'
     })
 }]);
+
+// change otherwise to 404, 500
