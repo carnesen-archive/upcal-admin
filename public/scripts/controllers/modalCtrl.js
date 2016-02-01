8a1529d3460a8f6ae09ee3a510dc032afe9d41c9
Andrew Dourgarian <andrewdourgarian@yahoo.com>
2016-02-01 11:00:52 -0600

    modal work

--- public/scripts/controllers/modalCtrl.js
+++ public/scripts/controllers/modalCtrl.js
@@ -1,38 +1,5 @@
-app.controller('modalCtrl', function ($scope, $uibModal, $log){
-
-  $scope.items = ['item1', 'item2', 'item3'];
-
-  $scope.animationsEnabled = true;
-
-  $scope.open = function (size) {
-
-    var modalInstance = $uibModal.open({
-      animation: $scope.animationsEnabled,
-      templateUrl: 'myModalContent.html',
-      controller: 'ModalInstanceCtrl',
-      size: size,
-      resolve: {
-        items: function () {
-          return $scope.items;
-        }
-      }
-    });
-
-    modalInstance.result.then(function (selectedItem) {
-      $scope.selected = selectedItem;
-    }, function () {
-      $log.info('Modal dismissed at: ' + new Date());
-    });
-  };
-
-  $scope.toggleAnimation = function () {
-    $scope.animationsEnabled = !$scope.animationsEnabled;
-  };
-
-});
 
 app.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance){
-
   $scope.cancel = function () {
     $uibModalInstance.dismiss('cancel');
   };
