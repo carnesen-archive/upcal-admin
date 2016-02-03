47709d0588471cb3777ab4baeb81484356be0b0e
Andrew Dourgarian <andrewdourgarian@yahoo.com>
2016-02-03 13:19:17 -0600

    google auth button

--- public/scripts/controllers/loginCtrl.js
+++ public/scripts/controllers/loginCtrl.js
@@ -1,3 +1,32 @@
-app.controller('loginCtrl', ['$scope', function ($scope){
-  $scope.message = 'Consumer Cue Generator.';
+app.controller('loginCtrl', ['$scope', '$http', 'authService', '$location',
+  function ($scope, $http, authService, $location) {
+
+    function onSignIn(googleUser) {
+      var profile = googleUser.getBasicProfile();
+      console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+      console.log('Name: ' + profile.getName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail());
+    }
+
+    function signOut() {
+      var auth2 = gapi.auth2.getAuthInstance();
+      auth2.signOut().then(function () {
+        console.log('User signed out.');
+      });
+    }
+
+  $http.post('api/login', $scope.form)
+    .then(function (response) {
+
+
+      // save json web token in session storage
+      authService.saveToken(response.data);
+
+      // redirect to projects page
+      $location.path('/bridge/' + $scope.form.username);
+    }, function () {
+      // wipe out the stored token
+      authService.logout();
+    })
 }]);
---
+++ public/scripts/factoriesServices/authInterceptor.js
@@ -0,0 +1,22 @@
+app.factory('authInterceptor', ['$q', '$location', 'authService', function ($q, $location, authService) {
+  return {
+    request: function (config) {
+      config.headers = config.headers || {};
+      if (authService.isAuthed()) {
+        config.headers.Authorization = 'Bearer ' + authService.getToken();
+      }
+      return config;
+    },
+    response: function (response) {
+      if (response.status === 401) {
+
+        // delete the token
+        authService.logout();
+
+        // handle the case where the user is not authenticated
+        $location.path("/login");
+      }
+      return response || $q.when(response);
+    }
+  };
+}]);
