b999b9061921ef6727f9fb81e1748af82be2856a
Andrew Dourgarian <andrewdourgarian@yahoo.com>
2016-02-06 10:26:10 -0600

    login function

--- public/scripts/controllers/loginCtrl.js
+++ public/scripts/controllers/loginCtrl.js
@@ -1,19 +1,24 @@
 app.controller('loginCtrl', ['$scope', '$http', '$location',
   function ($scope, $http, $location) {
 
-    $http.get('/api/clientInfo').then(function(response){
-      var clientID = response.data.clientID;
-      hello.init(
-        {google : clientID },
-        {redirect_uri:'http://localhost:3000/#/ccTable'}
-      );
-      login();
-    });
-
+    $scope.login = function(){
+      $http.get('/api/clientInfo').then(function(response){
+        var clientID = response.data.clientID;
+        console.log(clientID);
+        hello.init(
+          {google : clientID },
+          {redirect_uri:'http://localhost:3000'}
+        );
+        login();
+      });
+    };
     function login() {
       hello( 'google' ).login( function() {
         var token = hello( 'google' ).getAuthResponse().access_token;
+        $location.path('/ccTable.html');
         console.log(token)
+      }, function(e){
+        alert('login failed. Error:', e);
       });
     }
 
